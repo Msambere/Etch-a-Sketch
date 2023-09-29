927730eded1f9f0a9dd046dab4209e5dfcad7960
Msambere <110812972+Msambere@users.noreply.github.com>
2023-09-29 11:31:15 -0400

    Add hover effect
    Shaded class is added when divs are mouse-overed. Container deminsions
    and box count are temporarily hard coded.

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('#screen');
 
-let x = prompt("How many columns?");
-let y = prompt ("How many rows?");
-let boxCount = x*y;
+//let x = prompt("How many rows/columns?");
+//let boxCount = x*x;
+//let setFlexBasis = 100/x;
 
 function gridMaker(boxCount){
     for (i=0; i<boxCount; i++){
@@ -12,8 +12,15 @@ function gridMaker(boxCount){
     };
 };
 
+function addShading(e){
+    let gridBox = e.target;
+    gridBox.classList.add('shaded');
+}
 
-gridMaker(boxCount);
+
+gridMaker(16*16);
+const gridBoxes = Array.from(document.querySelectorAll('.gridBox'));
+gridBoxes.forEach(box => box.addEventListener('mouseover', addShading));
 
 /*
 TDL:
